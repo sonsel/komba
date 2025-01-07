530d6f5e4fbd39f0040de676a25d444993237746
sonsel <armando.komba@gmail.com>
2025-01-06 23:42:37 -0800

    1st version

---
+++ index.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+
+	var std = Std{Name: "armando", Sex: "male"}
+	b, _ := json.Marshal(std)
+	res, err := http.Post("http://localhost:90", "application/json", bytes.NewReader(b))
+	if err != nil {
+		panic(err.Error())
+	}
+	byt, _ := io.ReadAll(res.Body)
+	fmt.Println("request already sent")
+	fmt.Println(string(byt))
+
+	defer res.Body.Close()
+
+}
